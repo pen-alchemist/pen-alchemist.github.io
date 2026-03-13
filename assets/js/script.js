6f8aa9692d9232414d1bdd13902c813cf6279c3c
Yehor Romanov <lelouch.romanov@gmail.com>
2026-03-13 17:45:23 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,4 +39,21 @@ document.addEventListener('DOMContentLoaded', () => {
       updateInterface(event.matches? 'dark' : 'light');
     }
   });
+
+  const observerOptions = {
+    threshold: 0.1,
+    rootMargin: "0px 0px -20px 0px"
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll('tr').forEach(card => {
+    observer.observe(card);
+  });
 });
